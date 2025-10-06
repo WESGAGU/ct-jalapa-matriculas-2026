659773db7b2f4af64a33ad5b47355fe8e925028b
WESGAGU <weslin2016garcia@gmail.com>
2025-10-06 11:46:45 -0600

    feat: agregar soporte para análisis de uso con Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { cn } from '@/lib/utils';
 import { Toaster } from '@/components/ui/toaster';
 import SyncStatus from '@/components/sync-status';
 import { ThemeProvider } from '@/components/theme-provider';
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
 
@@ -33,6 +34,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {/* Aquí solo deben estar los hijos y los proveedores globales */}
+          <Analytics/>
           {children}
           <Toaster />
           <SyncStatus />
